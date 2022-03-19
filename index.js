aa3ff563ddd6885b0fe25264a2c960e27bb80341
Kevinacid <1589635180@qq.com>
2022-03-19 22:57:41 +0800

    初步完善github登录

---
+++ index.js
@@ -0,0 +1,75 @@
+const clientID = "15f99dde795d4bc02da2";
+const clientSecret = "583652a80005edfb0045025532c1cfe39209bcd0";
+
+const Koa = require("koa");
+const path = require("path");                        //Node.js Path 模块
+const serve = require("koa-static");              //处理静态资源
+const route = require("koa-route");              //路由
+const axios = require("axios");                     //创建、发送请求
+
+const app = new Koa();
+
+const main = serve(path.join(__dirname));
+
+// const oauth = async context => {
+//     const code = context.request.query.code;
+//
+//     const getAccessToken = await axios({
+//         method: "POST",
+//         url: "https://github.com/login/oauth/access_token?client_id=" + `${clientID}` + "&client_secret=" + `${clientSecret}` + "&code=" + `${code}`,
+//         headers: {
+//             accept: "application/json"
+//         }
+//     });
+//
+//     const accessToken = getAccessToken.data.access_token;
+//
+//     const result = await axios({
+//         method: "GET",
+//         url: "https://api.github.com/user",
+//         headers:{
+//             accept: "application/json",
+//             authorization: "token " + `${accessToken}`
+//         }
+//     });
+//
+//     const username = result.data.name;
+//
+//     context.response.redirect("/public/html/Pony.html?username=" + `${username}`);
+// };
+//
+// app.use(main);
+// app.use(route.get("oauth/redirect", oauth()));
+// app.listen(8080);
+
+const oauth = async context => {
+    const code = context.request.query.code;
+    console.log(code);
+
+    const getAccessToken = await axios({
+        method: 'POST',
+        url: "https://github.com/login/oauth/access_token?client_id=" + `${clientID}` + "&client_secret=" + `${clientSecret}` + "&code=" + `${code}`,
+        headers: {
+            accept: 'application/json'
+        }
+    });
+
+    const accessToken = getAccessToken.data.access_token;
+
+    const result = await axios({
+        method: 'GET',
+        url: "https://api.github.com/user",
+        headers: {
+            accept: 'application/json',
+            authorization: "token " + `${accessToken}`
+        }
+    });
+
+    const username = result.data.login;
+
+    context.response.redirect("/public/html/Pony.html?username=" + `${username}`);
+};
+
+app.use(main);
+app.use(route.get('/oauth/redirect', oauth));
+app.listen(8080);
