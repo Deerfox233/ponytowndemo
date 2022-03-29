e4e39f5c13ae29bcc11d77cb9cc71a737b003d72
SYH <2230692949@qq.com>
2022-03-29 18:02:59 +0800

    syh第一次

---
+++ src/js/about-pay.js
@@ -0,0 +1,8 @@
+function displayWindows() {
+    document.getElementById('window').style.display="block";
+    document.getElementById('shadow').style.display="block";
+}
+function hideWindows() {
+    document.getElementById('window').style.display="none";
+    document.getElementById('shadow').style.display="none";
+}
