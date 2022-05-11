b724bc1b2a2bd44062e95c3df8620b4e32c34d64
Kevinacid <1589635180@qq.com>
2022-05-11 10:04:52 +0800

    改bug

--- src/chat.ts
+++ src/chat.ts
@@ -1,13 +1,20 @@
 const webSocket: WebSocket = new WebSocket("ws://10.62.48.132:8080");
 
 let chatBox: HTMLElement = document.getElementById("div_chatBox");
+let chatArea = document.getElementById("chatArea_text");
 
 webSocket.onopen = function (event): void {
     console.log("websocket connection established");
 };
 
 webSocket.onmessage = function (event): void {
-    console.log("%s", JSON.parse(event.data).data);
+    let chars: number[];
+    chars = JSON.parse(event.data).data;
+    let s: string = "";
+    for (let i: number = 0; i < chars.length; i++) {
+        s += String.fromCharCode(chars[i]);
+    }
+    chatArea.innerHTML += "<span>" + s + "</span><br>";
 };
 
 function sendMessage(): void {
